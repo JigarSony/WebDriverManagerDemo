eff2ce8ed80302deb31256f90e8b85252176c425
Jigar Soni <sonijigar94@gmail.com>
2019-02-23 01:57:56 +0530

    Update WDMMain.java

--- src/main/java/WebDriverManagerDemo/WDMDemo/WDMMain.java
+++ src/main/java/WebDriverManagerDemo/WDMDemo/WDMMain.java
@@ -10,6 +10,7 @@ public class WDMMain {
 	public static void main(String[] args) {
 		
 		WebDriverManager.chromedriver().setup();
+		//WebDriverManager.chromedriver().version("2.45").setup();
 		
 		WebDriver driver = new ChromeDriver();
 		
